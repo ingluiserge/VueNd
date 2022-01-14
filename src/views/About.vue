d6ee34d75d4982c06635b7084902343efa5f51a4
luis-serge <luis-serge@hotmail.com>
2022-01-14 17:19:39 -0500

    sbpa

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="About">
+    <h1>This is an 2yv5t5t52v 5 page</h1>rgrgrg
+    wrferftgtg g
+     f
+     g
+      435
+      g 
+
+  </div>
+</template>
+ 
---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <br>
+    </div>
+    <div class=" row d-flex justify-content-center align-items-center ">
+      <div class="col">
+        <img class="image-responsive" style="width:300px;margin:0px;" src="../assets/pikachu-removebg-preview.png">
+      </div>
+      <div>
+        <br>
+      </div>
+      <div class="row">
+        <br>
+      </div>
+      <div class="row">
+        <strong class="col d-flex justify-content-center align-items-center" style="font-family: Lato;font-style: normal;font-weight: bold;font-size: 26px;line-height: 31px;display: flex;align-items: center;text-align: center;color: #353535;"> Welcome to Pokédex </strong>
+      </div>
+      <div>
+        <br>
+      </div>
+      <div class="row">
+        <h6 class="col align-self-center" style="Font family: Lato; Font style: Medium;Font size: 18px;Line height: 27px;Line height: 150%;Align: Center;Vertical align: Top;color: #5E5E5E;">
+          The digital encyclopedia created by Professor Oak is an invaluable tool
+        </h6>
+        <h6 style="Font family: Lato; Font style: Medium;Font size: 18px;Line height: 27px;Line height: 150%;Align: Center;Vertical align: Top;color: #5E5E5E;">
+          to Trainers in the Pokémon world.
+        </h6>
+      </div>
+      <div>
+        <br>
+      </div>
+      <div>
+        <br>
+      </div>
+      <div class="row">
+        <div class="col align-self-center">
+          <button type="button" class="btn btn-primary btn" style="border-radius: 50px;background: #F22539;border-style: solid;border-color:#F22539;">
+
+            <div class="col" style="Font family: Lato;color:#FFFFFF;">
+              <router-link style="text-decoration:none;color:#FFFFFF;" to="../views/About.vue"> Get started </router-link>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<!-- <script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>  -->
